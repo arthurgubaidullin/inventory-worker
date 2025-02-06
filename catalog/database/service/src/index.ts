f0e4f06b00205169dd9e53dead306b38f9c99606
Artur Gubaidullin <lapteuh@gmail.com>
2025-02-06 19:48:27 +0000

    Add interfaces

--- catalog/database/service/src/index.ts
+++ catalog/database/service/src/index.ts
@@ -1,1 +1,9 @@
-export const hello = "world";
+export interface CatalogItem {
+	readonly id: string;
+	readonly name: string;
+}
+
+export interface CatalogService {
+	readonly addItem: (item: CatalogItem) => Promise<void>;
+	readonly getItems: () => Promise<ReadonlyArray<CatalogItem>>;
+}
