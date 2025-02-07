9011896c2da9c36cf267c5d2385269c62981b481
Artur Gubaidullin <lapteuh@gmail.com>
2025-02-07 19:05:43 +0000

    Fix inventory worker

--- worker/src/inventory.ts
+++ worker/src/inventory.ts
@@ -1,4 +1,4 @@
-import { create, fromJson, toJsonString } from "@bufbuild/protobuf";
+import { create, fromJson, toJson, toJsonString } from "@bufbuild/protobuf";
 import * as InMemoryServices from "@inventory-worker/in-memory-services";
 import {
 	ActionSchema,
@@ -14,31 +14,37 @@ const app = new Hono();
 app.post("/actions", async (c) => {
 	const action = fromJson(ActionSchema, await c.req.json());
 
-	if ((action.type = ActionType.RECEIVED_GOODS)) {
+	if (action.type === ActionType.RECEIVED_GOODS) {
 		await inventory.receivedGoods(action);
 	}
-	if ((action.type = ActionType.SOLD_GOODS)) {
+
+	if (action.type === ActionType.SOLD_GOODS) {
 		await inventory.soldGoods(action);
 	}
 
-	c.status(202);
+	return new Response(null, {
+		status: 202,
+		headers: { "Content-Type": "application/json" },
+	});
 });
 
 app.get("/:id", async (c) => {
 	const catalogId = c.req.param("id");
 
-	const item = await inventory.getCurrentStock(catalogId);
+	const inventoryItem = await inventory.getCurrentStock(catalogId);
 
-	if (!item) {
+	if (!inventoryItem) {
 		return new Response(null, { status: 410 });
 	}
 
 	const json = toJsonString(
 		InventoryItemSchema,
-		create(InventoryItemSchema, item),
+		create(InventoryItemSchema, inventoryItem),
 	);
 
-	c.json(json);
+	return new Response(json, {
+		headers: { "Content-Type": "application/json" },
+	});
 });
 
 export default app;
