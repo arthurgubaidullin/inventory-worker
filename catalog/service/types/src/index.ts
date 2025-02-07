48130f56d5d04ee2ab24118fc02770206f024c03
Artur Gubaidullin <lapteuh@gmail.com>
2025-02-07 08:18:11 +0000

    Add types

--- catalog/service/types/src/index.ts
+++ catalog/service/types/src/index.ts
@@ -1,1 +1,14 @@
-export const lol = "lol";
+export interface CatalogItem {
+	readonly id: string;
+	readonly name: string;
+}
+
+export interface AddCatalogItem {
+	readonly addCatalogItem: (item: CatalogItem) => Promise<CatalogItem>;
+}
+
+export interface GetCatalogItems {
+	readonly getCatalogItems: () => Promise<CatalogItem[]>;
+}
+
+export type CatalogService = AddCatalogItem & GetCatalogItems;
