dad0572a5b62ca93b0abae0dc9212747434836db
Artur Gubaidullin <lapteuh@gmail.com>
2025-02-07 14:04:40 +0000

    Fix inventory service

--- inventory/service/factory/src/index.ts
+++ inventory/service/factory/src/index.ts
@@ -1,7 +1,11 @@
 import type { InventoryService } from "@inventory-worker/inventory-service-types";
+import type { InventoryDatabase } from "@inventory-worker/inventory-database-types";
 import { type LoggerService } from "@inventory-worker/logger-types";
 
-export const create = (logger: LoggerService): InventoryService => {
+export const create = (
+	db: InventoryDatabase,
+	logger: LoggerService,
+): InventoryService => {
 	return {
 		getCurrentStock: async (catalogId) => {
 			return { catalogId, quantity: 0 };
