9a69b9a27833b0ebbea1a198483fa83c62087eb9
Artur Gubaidullin <lapteuh@gmail.com>
2025-02-07 14:05:01 +0000

    Add inventory service to services

--- services/in-memory/src/create.ts
+++ services/in-memory/src/create.ts
@@ -1,5 +1,7 @@
 import * as CatalogServiceFactory from "@inventory-worker/catalog-service-factory";
 import * as InMemoryCatalogDatabase from "@inventory-worker/in-memory-catalog-database";
+import * as InMemoryInventoryDatabase from "@inventory-worker/in-memory-inventory-database";
+import * as InventoryServiceFactory from "@inventory-worker/inventory-service-factory";
 import * as LoggerFactory from "@inventory-worker/logger-factory";
 import type { Services } from "@inventory-worker/services-types";
 
@@ -8,10 +10,15 @@ export const create = (): Services => {
 
 	const catalogDatabase = InMemoryCatalogDatabase.getInMemoryCatalogService();
 
+	const inventoryDatabase = InMemoryInventoryDatabase.get();
+
 	const catalog = CatalogServiceFactory.create(catalogDatabase, logger);
+
+	const inventory = InventoryServiceFactory.create(inventoryDatabase, logger);
 
 	return {
 		catalog,
+		inventory,
 		logger,
 	};
 };
--- services/types/src/types.ts
+++ services/types/src/types.ts
@@ -1,12 +1,17 @@
 import { type CatalogService } from "@inventory-worker/catalog-service-types";
+import { type InventoryService } from "@inventory-worker/inventory-service-types";
 import { type LoggerService } from "@inventory-worker/logger-types";
 
 export interface Catalog {
 	readonly catalog: CatalogService;
 }
 
+export interface Inventory {
+	readonly inventory: InventoryService;
+}
+
 export interface Logger {
 	readonly logger: LoggerService;
 }
 
-export type Services = Catalog & Logger;
+export type Services = Catalog & Inventory & Logger;
